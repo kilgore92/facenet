da844f338da0019a17f3eb5ed74ef2020a5947ed
kilgore92 <ishaan.bits92@gmail.com>
2018-04-05 15:08:55 +0200

    Import align function from compare.py

--- src/gans_compare.py
+++ src/gans_compare.py
@@ -35,6 +35,7 @@ import os
 import argparse
 import facenet
 import align.detect_face
+from compare import *
 
 def main(args):
 
@@ -79,43 +80,6 @@ def main(args):
                 model_name = image_paths[i].split('/')[-1].split('.')[0]
                 dist = np.sqrt(np.sum(np.square(np.subtract(emb[0,:], emb[i,:]))))
                 print('{} :: {}'.format(model_name.upper(),dist))
-
-
-def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
-
-    minsize = 20 # minimum size of face
-    threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
-    factor = 0.709 # scale factor
-
-    with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction,visible_device_list = "0")
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-        with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
-
-    tmp_image_paths = image_paths.copy()
-    img_list = []
-    for image in tmp_image_paths:
-        print(os.path.expanduser(image))
-        img = misc.imread(os.path.expanduser(image), mode='RGB')
-        img_size = np.asarray(img.shape)[0:2]
-        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
-        if len(bounding_boxes) < 1:
-          image_paths.remove(image)
-          print("can't detect face, remove ", image)
-          continue
-        det = np.squeeze(bounding_boxes[0,0:4])
-        bb = np.zeros(4, dtype=np.int32)
-        bb[0] = np.maximum(det[0]-margin/2, 0)
-        bb[1] = np.maximum(det[1]-margin/2, 0)
-        bb[2] = np.minimum(det[2]+margin/2, img_size[1])
-        bb[3] = np.minimum(det[3]+margin/2, img_size[0])
-        cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
-        aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
-        prewhitened = facenet.prewhiten(aligned)
-        img_list.append(prewhitened)
-    images = np.stack(img_list)
-    return images
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
